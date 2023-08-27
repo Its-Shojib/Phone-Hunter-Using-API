7a04000527e8f83a513171c54306a043b1151407
Its_Shojib <mdshojib922@gmail.com>
2023-08-28 03:36:20 +0600

    Modals Added

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function ShowPhone(phones, isShowAll) {
         document.getElementById("showAll").classList.add("hidden");
     }
 
-    console.log(isShowAll);
+    // Slice to 12 card
     if (!isShowAll) {
         phones = phones.slice(0, 12);
     }
@@ -35,9 +35,8 @@ function ShowPhone(phones, isShowAll) {
         <div class="card-body items-center text-center">
             <h2 class="card-title">${phones.brand}</h2>
             <p>${phones.phone_name}</p>
-            <p>${phones.slug}</p>
             <div class="card-actions">
-            <button class="btn btn-primary">Details</button>
+            <button onclick="ShowDetails('${phones.slug}')" class="btn btn-primary">Details</button>
         </div>`;
         container.appendChild(phoneCard);
     });
@@ -50,6 +49,35 @@ function Search(isShowAll) {
     let searchValue = searchField.value;
     loadData(searchValue, isShowAll);
 }
+
+let ShowDetails = async (id) => {
+    let res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    let data = await res.json();
+    //   console.log(data);
+    let details = data.data;
+    PhoneDetails(details);
+}
+let PhoneDetails = (details) => {
+    console.log(details);
+    let phoneName = document.getElementById("PhoneName");
+    phoneName.textContent = details.name;
+    let ShowDetailsContainer = document.getElementById("ShowDetailsContainer");
+    ShowDetailsContainer.innerHTML = `
+    <img src="${details.image}" class="mx-auto">
+    <p><span>Storage:</span>${details?.mainFeatures?.storage}</p>
+    <p><span>GPS:</span>${details.others?.GPS || 'No GPS available'}</p>
+    <p><span>GPS:</span>${details.others?.GPS ? details.others.GPS : 'No GPS available in this device'}</p>F
+    `;
+
+
+
+
+    my_modal.showModal()
+}
+
+
+
+
 let toggleLoadingSpinner = (loading) => {
     let loadingSpinner = document.getElementById("loading-spinner");
     if (loading) {
