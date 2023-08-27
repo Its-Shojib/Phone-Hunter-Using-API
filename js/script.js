43a881afd47222e541688f0b769f217acc3c42f5
Its_Shojib <mdshojib922@gmail.com>
2023-08-28 02:52:40 +0600

    Show All Added

--- js/script.js
+++ js/script.js
@@ -1,24 +1,34 @@
-let loadData = async (searchValue) =>{
+let loadData = async (searchValue, isShowAll) => {
     let res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchValue}`);
     let data = await res.json();
     let phones = data.data;
     // console.log(phones);
-    ShowPhone(phones);
+    ShowPhone(phones, isShowAll);
 
 }
 
 
-function ShowPhone(phones){
+function ShowPhone(phones, isShowAll) {
     let container = document.getElementById("container")
     container.textContent = '';
-    phones = phones.slice(0,10);
+    if (phones.length > 12 && !isShowAll) {
+        document.getElementById("showAll").classList.remove("hidden");
+    }
+    else {
+        document.getElementById("showAll").classList.add("hidden");
+    }
+
+    console.log(isShowAll);
+    if (!isShowAll) {
+        phones = phones.slice(0, 12);
+    }
 
 
     container.classList = "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5"
     phones.forEach(phones => {
         let phoneCard = document.createElement("div")
         phoneCard.classList = "card bg-gray-400 shadow-xl";
-        phoneCard.innerHTML =`
+        phoneCard.innerHTML = `
         <figure class="px-10 pt-10">
             <img src="${phones.image}" />
         </figure>
@@ -31,12 +41,27 @@ function ShowPhone(phones){
         </div>`;
         container.appendChild(phoneCard);
     });
+    toggleLoadingSpinner(false)
 }
 
-function Search(){
+function Search(isShowAll) {
+    toggleLoadingSpinner(true);
     let searchField = document.getElementById("searchPhones");
     let searchValue = searchField.value;
-    // console.log(searchValue);
-    loadData(searchValue);
-    searchField.value = "";
+    loadData(searchValue, isShowAll);
 }
+let toggleLoadingSpinner = (loading) => {
+    let loadingSpinner = document.getElementById("loading-spinner");
+    if (loading) {
+        loadingSpinner.classList.remove("hidden");
+    }
+    else {
+        loadingSpinner.classList.add("hidden");
+    }
+}
+
+let ShowAll = () => {
+    Search(true);
+}
+
+// loadData();
