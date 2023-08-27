0a3266a3a6f417fb9301a709d115f41691345239
Its_Shojib <mdshojib922@gmail.com>
2023-08-28 01:06:14 +0600

    Phone SHowed 10

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
-let loadData = async () =>{
-    let res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+let loadData = async (searchValue) =>{
+    let res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchValue}`);
     let data = await res.json();
     let phones = data.data;
     // console.log(phones);
     ShowPhone(phones);
 
 }
-loadData();
+
 
 function ShowPhone(phones){
     let container = document.getElementById("container")
+    container.textContent = '';
+    phones = phones.slice(0,10);
+
+
     container.classList = "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5"
     phones.forEach(phones => {
-        console.log(phones);
         let phoneCard = document.createElement("div")
         phoneCard.classList = "card bg-gray-400 shadow-xl";
         phoneCard.innerHTML =`
@@ -31,5 +34,9 @@ function ShowPhone(phones){
 }
 
 function Search(){
-    
+    let searchField = document.getElementById("searchPhones");
+    let searchValue = searchField.value;
+    // console.log(searchValue);
+    loadData(searchValue);
+    searchField.value = "";
 }
